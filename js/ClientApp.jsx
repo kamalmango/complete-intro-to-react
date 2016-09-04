56bb735accd2327ac27b771b550ada558f683866
Kamal Mango <kamal.mango7@gmail.com>
2016-09-04 20:52:20 +0200

    add react router

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,16 +1,22 @@
 // this is the main file beucase it doesnt have an export (webpack purporses)
 const React = require('react')
 const ReactDOM = require('react-dom')
+const Landing = require('./Landing')
+const Search = require('./Search')
+const {Router, Route, hashHistory} = require('react-router') //destructuring es6
+
+// const {Router, Route, hashHistory} = ReactRouter
+// OR (equivalent to)
+// const Router = ReactRouter.Router
+// const Route = ReactRouter.Route
+// const hashHistory = ReactRouter.hashHistory
 
 //stateless component
 const App = () => (
-  <div className='app-container'> 
-    <div className='home-info'>
-      <h1 className='title'>svideo</h1>
-      <input className='search' type='text' placeholder='Search' />
-      <button className='browse-all'> or Browse All </button>
-    </div>
-  </div>
+  <Router history={hashHistory}>
+    <Route path='/' component={Landing} />
+    <Route path='/search' component={Search} />
+  </Router>
 )  
 
 
